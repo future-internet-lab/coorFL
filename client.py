dff1568faa2c7957c29a6860fc0b0bafa96f384a
MINHDANGab <thaotrau55@gmail.com>
2025-03-19 12:09:07 +0700

    add case domain2

--- client.py
+++ client.py
@@ -23,6 +23,7 @@ client_id = uuid.uuid4()
 address = config["rabbit"]["address"]
 username = config["rabbit"]["username"]
 password = config["rabbit"]["password"]
+data_name = config["server"]["data-name"]
 
 device = None
 
@@ -41,27 +42,33 @@ else:
 credentials = pika.PlainCredentials(username, password)
 
 
-def train_on_device(model, lr, momentum, trainloader, criterion, clip_grad_norm=None):
+def train_on_device(model, lr, momentum, trainloader, criterion, epoch = 1, clip_grad_norm=None):
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
     model.train()
-    for (training_data, label) in tqdm(trainloader):
-        if training_data.size(0) == 1:
-            continue
-        training_data = training_data.to(device)
-        label = label.to(device)
-        optimizer.zero_grad()
-        output = model(training_data)
-        loss = criterion(output, label)
+    for _ in range(epoch):
+        for (training_data, label) in tqdm(trainloader):
+            if training_data.size(0) == 1:
+                continue
+            training_data = training_data.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            if data_name == "DOMAIN2":
+                output = model(training_data)
+                output = torch.softmax(output, dim=1)[:, 1]
+                loss = criterion(output, label.float())
+            else:
+                output = model(training_data)
+                loss = criterion(output, label)
 
-        if torch.isnan(loss).any():
-            src.Log.print_with_color("NaN detected in loss, stop training", "yellow")
-            return False
+            if torch.isnan(loss).any():
+                src.Log.print_with_color("NaN detected in loss, stop training", "yellow")
+                return False
 
-        loss.backward()
-        if clip_grad_norm and clip_grad_norm > 0:
-            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_grad_norm)
-        optimizer.step()
+            loss.backward()
+            if clip_grad_norm and clip_grad_norm > 0:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), clip_grad_norm)
+            optimizer.step()
 
     return True
 
