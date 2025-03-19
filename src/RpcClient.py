2836b118c5f67b840600512ca6cbe53efdd18e81
MINHDANGab <thaotrau55@gmail.com>
2025-03-19 21:15:14 +0700

    change transforms CIFAR 10

--- src/RpcClient.py
+++ src/RpcClient.py
@@ -102,7 +102,7 @@ class RpcClient:
                         transforms.RandomCrop(32, padding=4),
                         transforms.RandomHorizontalFlip(),
                         transforms.ToTensor(),
-                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                     ])
                     self.train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
                                                                   transform=transform_train)
