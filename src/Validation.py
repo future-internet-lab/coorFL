661edb184051cf7262d01a64f3081aae5c4d0104
MINHDANGab <thaotrau55@gmail.com>
2025-03-19 12:51:38 +0700

    update test_domain 2

--- src/Validation.py
+++ src/Validation.py
@@ -138,9 +138,9 @@ class Validation:
 
         # Calculate metrics
         accuracy = accuracy_score(all_labels, all_preds)
-        precision = precision_score(all_labels, all_preds, average='weighted', zero_division=0)
-        recall = recall_score(all_labels, all_preds, average='weighted', zero_division=0)
-        f1 = f1_score(all_labels, all_preds, average='weighted', zero_division=0)
+        precision = precision_score(all_labels, all_preds,zero_division=0)
+        recall = recall_score(all_labels, all_preds,  zero_division=0)
+        f1 = f1_score(all_labels, all_preds,  zero_division=0)
 
         # Log the results
         print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1-score: {f1:.4f}')
